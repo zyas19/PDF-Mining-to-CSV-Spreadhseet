0d7bd9f0f92d76497422ff9a4bb6861b7588744a
Yasmin Stoss <38015638+zyas19@users.noreply.github.com>
2019-04-24 11:21:43 -0400

    Update and rename pdf_mine_StossY.py to pdf_mine.py

--- pdf_mine.py
+++ pdf_mine.py
@@ -3,6 +3,7 @@ from tabula import convert_into
 import os
 # Python version 3.7.0
 # Microsoft Windows 10 Enterprise version 10.0.15063 Build 15063
+# Mac OS Mojave 10.14.3 (18D109)
 
 #///////extract information from table in a pdf and output into excel sheet/////////
 #useful blog from creator of tabula-py: https://blog.chezo.uno/tabula-py-extract-table-from-pdf-into-python-dataframe-6c7acfa5f302
@@ -11,7 +12,7 @@ import os
 def extract_budget ():
 
     # area and columns define the location coordinates of the table in pdf units
-    # If an error occurs, compare this years DOE Budget report to the 2020 report to see if the table locations are different
+    # If an error occurs, compare this years report to the example report to see if the table locations are different
     # pages need to be updated each year
     # useful thread on defining coordinates: https://stackoverflow.com/questions/45457054/tabula-extract-tables-by-area-coordinates
     output = convert_into(filePDF, outpath, guess = False, area = (86.13,43.32,706.71,554.48), columns = (43.32,341.02,405,478.26,554.48), output_format = "csv", pages = '5-137')
